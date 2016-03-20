b603072bb83ee1ccf6c4ec70a85b60b1d0fbf9e6
us3rn4me <Korons@vfemail.net>
2016-03-20 00:33:03 -0400

    Added -a option for adding mods and -l for listing mods

--- openmmm.py
+++ openmmm.py
@@ -3,26 +3,47 @@
 import sys
 import os
 import shutil
-if len(sys.argv) == 1:
-    help_message = '{0} YourModFolder'.format(sys.argv[0])
-    print (help_message)
-    sys.exit()
+import re
+import argparse
+
+parser = argparse.ArgumentParser(description='Manage openMW mods')
+parser.add_argument("-a", help="Mod to add",)
+parser.add_argument("-l", help="List mods", action="store_true")
+args = parser.parse_args()
+
+
+# if len(sys.argv) == 1:
+#     help_message = '{0} YourModFolder'.format(sys.argv[0])
+#     print (help_message)
+#     sys.exit()
 
 home = os.getenv("HOME")
 
-mod_name = sys.argv[1]
-
 openmw_config_path = home + '/.config/openmw/'
 openmw_config_file_path = home + '/.config/openmw/openmw.cfg'
-mod_dir = home + '/MWmods/'
-mod_dir_copy_path = home + '/MWmods/' + mod_name
-data_string = 'data="{0}{1}"'.format(mod_dir, mod_name)
 
-os.makedirs(mod_dir,exist_ok=True)
-shutil.copytree(mod_name, mod_dir_copy_path)
+def list_mods():
+    with open(openmw_config_file_path) as f:
+        print ('Opening config ifle')
+        lines = f.read().splitlines()
+    for i in lines:
+        if bool(re.findall("data=", i)) == True:
+            print (i)
 
-# We make a back up of the config file
-shutil.copy(openmw_config_file_path, openmw_config_path + 'openmw.cfg.bak')
+if args.a:
+    mod_name = args.a
+    mod_dir = home + '/MWmods/'
+    mod_dir_copy_path = home + '/MWmods/' + mod_name
+    data_string = 'data="{0}{1}"'.format(mod_dir, mod_name)
+    os.makedirs(mod_dir,exist_ok=True)
+    shutil.copytree(mod_name, mod_dir_copy_path)
 
-with open(openmw_config_file_path, mode='a') as config:
-    print (data_string, file=config)
+    # We make a back up of the config file
+    shutil.copy(openmw_config_file_path, openmw_config_path + 'openmw.cfg.bak')
+
+    with open(openmw_config_file_path, mode='a') as config:
+        print (data_string, file=config)
+
+
+elif args.l:
+    list_mods()
