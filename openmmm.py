ed1da715ba413ede2132f45f293ec989ee03a70a
Korons <Korons@vfemail.net>
2016-06-29 17:25:55 -0400

    PEP8 cleaning

--- openmmm.py
+++ openmmm.py
@@ -12,31 +12,32 @@ parser.add_argument("-l", help="List mods", action="store_true")
 args = parser.parse_args()
 
 home = os.getenv("HOME")
-
 openmw_config_path = home + '/.config/openmw/'
 openmw_config_file_path = home + '/.config/openmw/openmw.cfg'
 
+
 def list_mods():
     with open(openmw_config_file_path) as f:
-        print ('Opening config ifle')
+        print('Opening config file')
         lines = f.read().splitlines()
     for i in lines:
         if bool(re.findall("data=", i)) == True:
-            print (i)
+            print(i)
 
 if args.a:
     mod_name = args.a
+    # This is the dir your mods will be stored in
     mod_dir = home + '/MWmods/'
     mod_dir_copy_path = home + '/MWmods/' + mod_name
     data_string = 'data="{0}{1}"'.format(mod_dir, mod_name)
-    os.makedirs(mod_dir,exist_ok=True)
+    os.makedirs(mod_dir, exist_ok=True)
     shutil.copytree(mod_name, mod_dir_copy_path)
 
     # We make a back up of the config file
     shutil.copy(openmw_config_file_path, openmw_config_path + 'openmw.cfg.bak')
 
     with open(openmw_config_file_path, mode='a') as config:
-        print (data_string, file=config)
+        print(data_string, file=config)
 
 
 elif args.l:
